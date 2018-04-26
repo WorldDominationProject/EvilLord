c14b5047924e087cda38f45a10fe308df75ce863
stranametschty <stranametschty@gmail.com>
2018-04-26 21:25:38 +0200

    evil_throne facing on placement resolved #2

--- src/main/java/org/worlddominationproject/evillord/common/blocks/BlockEvilThrone.java
+++ src/main/java/org/worlddominationproject/evillord/common/blocks/BlockEvilThrone.java
@@ -2,15 +2,23 @@ package org.worlddominationproject.evillord.common.blocks;
 
 import org.worlddominationproject.evillord.EvilLordMod;
 
-import net.minecraft.block.Block;
+import net.minecraft.block.BlockHorizontal;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.state.BlockStateContainer;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 /**
  *
  * @author stranametschty
  *
  */
-public class BlockEvilThrone extends Block {
+public class BlockEvilThrone extends BlockHorizontal {
 
 	public static final String ID = "evil_throne";
 
@@ -18,5 +26,35 @@ public class BlockEvilThrone extends Block {
 		super(Material.ROCK);
 		setRegistryName(EvilLordMod.MODID, ID);
 		setUnlocalizedName("Evil Throne");
+		setDefaultState(this.blockState.getBaseState().withProperty(FACING, EnumFacing.NORTH));
 	}
+
+	@Override
+	public IBlockState getStateForPlacement(final World world, final BlockPos pos, final EnumFacing facing,
+			final float hitX, final float hitY, final float hitZ, final int meta, final EntityLivingBase placer,
+			final EnumHand hand) {
+		return super.getStateForPlacement(world, pos, facing, hitX, hitY, hitZ, meta, placer, hand).withProperty(FACING,
+				placer.getHorizontalFacing().rotateY());
+	}
+
+	@Override
+	protected BlockStateContainer createBlockState() {
+		return new BlockStateContainer(this, new IProperty[] { FACING });
+	}
+
+	@Override
+	public int getMetaFromState(final IBlockState state) {
+		return state.getValue(FACING).getIndex();
+	}
+
+	@Override
+	public IBlockState getStateFromMeta(final int meta) {
+		return getDefaultState().withProperty(FACING, EnumFacing.getHorizontal(meta));
+	}
+
+	@Override
+	public boolean isOpaqueCube(final IBlockState state) {
+		return false;
+	}
+
 }
